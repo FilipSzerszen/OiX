7a1f9e6be51553165ceaeb8453f5d6af1727ea5c
Fi Szer <filipsz60@wp.pl>
2023-04-19 20:56:22 +0200

    Dodaj pliki projektów.

---
+++ OiX/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace OiX
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartGry(object sender, EventArgs e)
+        {
+            Program.StartGry();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            _ = (Program.czyGracz1) ? ((Button)sender).Text = "X" : ((Button)sender).Text = "O";
+            ((Button)sender).Enabled = false;
+            Program.czyGracz1 ^= true;
+            if (Program.CzyWygrana() != 0)
+            {
+                if (Program.CzyWygrana() == 3) MessageBox.Show("Remis!", "Wynik gry", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                else MessageBox.Show("Wygrał gracz: " + Program.CzyWygrana(), "Wynik gry", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Program.StartGry();
+            }
+
+        }
+
+    }
+}
---
+++ OiX/Program.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OiX
+{
+    public static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+
+        }
+        public static int[,] plansza = new int[3, 3];
+
+        public static bool czyGracz1 = true;
+        public static int stan = 9;
+        public static void StartGry()
+        {
+            var Buttons = Form1.ActiveForm.Controls.OfType<Button>();
+            for (int i = 1; i < 10; i++)
+            {
+                var button = Buttons.Where(p => p.Name == "button" + i).FirstOrDefault();
+                if (button != null)
+                {
+                    button.Text = default;
+                    button.Enabled = true;
+                }
+            }
+            czyGracz1 = true;
+            
+        }
+        public static void CzyœæTablicê()
+        {
+            for(int i =0; i < 3; i++)
+            {
+                for(int j=0; j < 3; j++)
+                {
+                    plansza[i, j] = 0;
+                }
+            }
+        }
+
+        public static int CzyWygrana()
+        {
+            if (plansza[0, 0] + plansza[0, 1] + plansza[0, 2] == 3 ||
+                plansza[1, 0] + plansza[1, 1] + plansza[1, 2] == 3 ||
+                plansza[2, 0] + plansza[2, 1] + plansza[2, 2] == 3 ||
+                plansza[0, 0] + plansza[1, 0] + plansza[2, 0] == 3 ||
+                plansza[0, 1] + plansza[1, 1] + plansza[2, 1] == 3 ||
+                plansza[0, 2] + plansza[1, 2] + plansza[2, 2] == 3 ||
+                plansza[0, 0] + plansza[1, 1] + plansza[2, 2] == 3 ||
+                plansza[2, 0] + plansza[1, 1] + plansza[0, 2] == 3
+                ) return 1;
+            if (plansza[0, 0] + plansza[0, 1] + plansza[0, 2] == 30 ||
+                plansza[1, 0] + plansza[1, 1] + plansza[1, 2] == 30 ||
+                plansza[2, 0] + plansza[2, 1] + plansza[2, 2] == 30 ||
+                plansza[0, 0] + plansza[1, 0] + plansza[2, 0] == 30 ||
+                plansza[0, 1] + plansza[1, 1] + plansza[2, 1] == 30 ||
+                plansza[0, 2] + plansza[1, 2] + plansza[2, 2] == 30 ||
+                plansza[0, 0] + plansza[1, 1] + plansza[2, 2] == 30 ||
+                plansza[2, 0] + plansza[1, 1] + plansza[0, 2] == 30
+                ) return 2;
+            if (stan == 0) return 3;
+            return 0;
+        }
+
+        
+    }
+}
